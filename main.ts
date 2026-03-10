61d19ad9aefad1da93ad60b4af17d06a97cc3719
Brandon-Tsang <brandon.tsang@stu.ocsb.ca>
2026-03-10 16:42:48 -0400

    Shows series of LEDs

--- main.ts
+++ main.ts
@@ -19,30 +19,34 @@ input.onButtonPressed(Button.A, function() {
     // turn on red
     pins.digitalWritePin(DigitalPin.P13, 1)
     basic.showString('Red')
+
+    // turn off red
     basic.pause(1000)
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 0)
 
     // turn on green
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.showString('Green')
+
+    // turn off green
     basic.pause(1000)
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P14, 0)
 
     // turn on blue
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.showString('Blue')
+    
+    // turn off blue
     basic.pause(1000)
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P15, 0)
 
     // turn on magenta
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.showString('Magenta')
+    
+    // turn off magenta
     basic.pause(1000)
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
 
@@ -50,8 +54,9 @@ input.onButtonPressed(Button.A, function() {
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.showString('Cyan')
+    
+    // turn off cyan
     basic.pause(1000)
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
 
@@ -59,8 +64,9 @@ input.onButtonPressed(Button.A, function() {
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.showString('Yellow')
+    
+    // turn off yellow
     basic.pause(1000)
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
 
@@ -69,8 +75,9 @@ input.onButtonPressed(Button.A, function() {
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     basic.showString('White')
+    
+    // turn off white
     basic.pause(1000)
-    basic.clearScreen()
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
